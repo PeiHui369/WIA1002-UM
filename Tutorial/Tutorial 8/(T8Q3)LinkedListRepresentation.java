7c781626be7bb0632b2ade47a294ff32a618b01e
PeiHui369 <108777577+PeiHui369@users.noreply.github.com>
2023-05-24 09:25:11 +0800

    Ans

--- Tutorial/Tutorial 8/(T8Q3)LinkedListRepresentation.java
+++ Tutorial/Tutorial 8/(T8Q3)LinkedListRepresentation.java
@@ -102,3 +102,19 @@ class Graph <E extends Comparable<E>>{
 
     }
 }
+
+@
+String[][] vert = {"A", "B", "C", "D", "E", "F", "G", "H", "I");
+for(String str:vert){
+        myGraph.addVertex(str);
+       myGraph.addEdge("A", "C", 1);
+       myGraph.addEdge("A", "D", 1);
+       myGraph.addEdge("B", "D", 1);
+       myGraph.addEdge("C", "E", 1);
+       myGraph.addEdge("C", "F", 1);
+       myGraph.addEdge("D", "E", 1);
+       myGraph.addEdge("E", "G", 1);
+       myGraph.addEdge("F", "H", 1);
+       myGraph.addEdge("G", "H", 1);
+       myGraph.addEdge("H", "I", 1);   
+    
